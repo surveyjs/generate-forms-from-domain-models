415dcebcefe2552a40aa4b504eb43b03cd2b3066
Andrew Telnov <andrew.telnov@gmail.com>
2023-02-26 12:33:01 +0200

    Add NPSSurvey form

---
+++ DomainModels/NPSSurvey.cs
@@ -0,0 +1,22 @@
+﻿using SurveyJSAsFormLibrary.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SurveyJSAsFormLibrary.DomainModels
+{
+    [DomainModelForm("nps-survey", "NPS Survey (Domain Model without attributes)")]
+    public class NPSSurvey: DomainModel
+    {
+        public int NPSScore { get; set; }
+        public string DisappointingExperience { get; set; }
+        public string ImprovementsRequired { get; set; }
+        public List<string> PromoterFeatures { get; set; }
+        public bool Rebuy { get; set; }
+        public string Testimonial { get; set; }
+        public string Email { get; set; }
+    }
+}
