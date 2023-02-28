91c66c8aef5150d60bb9ba1d3a23dde512f79064
Andrew Telnov <andrew.telnov@gmail.com>
2023-02-28 13:22:34 +0200

    Add delay on storing data

--- wwwroot/js/surveyjs.js
+++ wwwroot/js/surveyjs.js
@@ -3,8 +3,12 @@
     survey.storeOthersAsComment = false;
     survey.completeText = "Submit";
     survey.showQuestionNumbers = false;
-    survey.navigateToUrl = "/";
     survey.onComplete.add((sender, options) => {
         saveFormData(formName, survey);
+        //It is better to do it on successful saving data callback from the server
+        setTimeout(() => {
+            //Go to index page
+            window.location.href = "/";
+        }, 1000);
     });
 }
